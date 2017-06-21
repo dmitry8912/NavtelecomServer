0d164633eaf6a07238c83b529db3010bfc90209f
dmitry8912 <dmitry8912@gmail.com>
2017-06-21 13:57:54 +0400

    Little performance improvements...

--- nvg_client.py
+++ nvg_client.py
@@ -2,13 +2,20 @@ from lib import navtelecom
 from lib import postgres
 from tendo import singleton
 import logging
+import time
 
 me = singleton.SingleInstance()
 ntc = navtelecom.Navtelecom()
-logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG, filename = u'NVGC_client.log')
+logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG, filename = u'/var/sr-nav/navtelecom/NVGC_client.log')
 while(True):
     t = (postgres.NavtelecomDB.getInstance()).getUnhandledPacketsCount()
+    logging.info('located '+str(t[0][0])+' enties')
     if(t[0][0] > 0):
+        logging.info('decoding')
         ntc.decodeFlexFromDB()
     else:
-        break
+        time.sleep(5)
+        t = (postgres.NavtelecomDB.getInstance()).getUnhandledPacketsCount()
+        if(t[0][0] < 0):
+            logging.info('SCRIPT END')
+            break
